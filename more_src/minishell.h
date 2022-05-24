5d9ca68594dcda18b230ea44c1cc0b056b17769d
Thome <thomazzribeiro@hotmail.com>
2022-05-24 02:05:07 -0300

    Makefile and lib -> broken

---
+++ more_src/minishell.h
@@ -0,0 +1,34 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../librarie/libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+typedef struct s_data
+{
+	int		exit_code;
+	int		exit;
+	int		error;
+}	t_data;
+
+extern t_data *global_data;
+
+int		ft_str_check(const char *s1, const char *s2);
+
+void	pwd_built_in(void);
+void	echo_built_in(char **cmd);
+
+void	kill_loop(int signum);
+void	quit_core(int signum);
+
+#endif
